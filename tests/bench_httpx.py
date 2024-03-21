96dc1c29ecb6f9336bd4f05e50f1b262f9c1ebc1
Sitt Guruvanich <aekazitt@gmail.com>
2024-03-21 15:47:47 +0700

    Bench against httpx

--- tests/bench_httpx.py
+++ tests/bench_httpx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.8
 # coding:utf-8
 # Copyright (C) 2024 All rights reserved.
-# FILENAME:    ~~/tests/bench.py
+# FILENAME:    ~~/tests/bench_httpx.py
 # VERSION: 	   0.1.0
 # CREATED: 	   2024-03-21 13:31
 # AUTHOR: 	   Sitt Guruvanich <aekazitt+github@gmail.com>
@@ -10,12 +10,14 @@
 # HISTORY:
 # *************************************************************
 
+### Third-party packages ###
+from httpx import Response, get
+
 ### Local modules ###
-from alcedo import Response, get
 from tests import setup_teardown_api_server
 
 
-def test_alcedo_get_generic_endpoint() -> None:
+def test_httpx_get_generic_endpoint() -> None:
     for _ in range(10_000):
         response: Response = get("http://localhost:6969")
-        assert response == "OK"
+        assert response.text == "OK"
