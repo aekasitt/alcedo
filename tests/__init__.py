cc3ebf54971bada97f770642a812f3fdf35b0468
Sitt Guruvanich <aekazitt@gmail.com>
2024-03-21 13:40:57 +0700

    Add topdoc

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.8
+# coding:utf-8
+# Copyright (C) 2024 All rights reserved.
+# FILENAME:    ~~/tests/__init__.py
+# VERSION: 	   0.1.0
+# CREATED: 	   2024-03-21 13:31
+# AUTHOR: 	   Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
